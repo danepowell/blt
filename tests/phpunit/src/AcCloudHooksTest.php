3cb6f9a6ffd16f755d06f51ae7c9000df7e9329f
Dane Powell <git@danepowell.com>
2020-03-18 13:56:29 -0700

    Actually got it working.

--- tests/phpunit/src/AcCloudHooksTest.php
+++ tests/phpunit/src/AcCloudHooksTest.php
@@ -16,35 +16,23 @@ class AcCloudHooksTest extends BuildTestBase {
   public function testSetupCloudHooks() {
     $this->copyCodebase();
 
-    $this->executeCommand('blt recipes:cloud-hooks:init');
+    $this->executeCommand($this->getDrupalRoot() . '/../vendor/bin/blt recipes:cloud-hooks:init');
     $this->assertCommandSuccessful();
 
-    $this->assertFileExists($this->getDrupalRoot() . '/hooks');
+    $this->assertFileExists($this->getDrupalRoot() . '/../hooks');
 
-    $commonPostCodeDeployScript = $this->getDrupalRoot() . '/hooks/common/post-code-deploy/post-code-deploy.sh';
+    $commonPostCodeDeployScript = $this->getDrupalRoot() . '/../hooks/common/post-code-deploy/post-code-deploy.sh';
     $this->assertFileExists($commonPostCodeDeployScript);
 
     $filePermissions = substr(sprintf('%o', fileperms($commonPostCodeDeployScript)), -4);
     $this->assertEquals('0755', $filePermissions);
 
-    $finder = new PhpExecutableFinder();
     // You can keep the process object for later assertions or manipulations.
     $this->executeCommand(
-      $finder->find() . ' ./core/scripts/drupal install minimal'
+      $this->getDrupalRoot() . '/core/scripts/drupal install minimal'
     );
     $this->assertCommandSuccessful();
 
-    // Mimics hooks/post-code-deploy/post-code-deploy.sh.
-    $this->executeCommand("blt artifact:ac-hooks:post-code-update", [
-      'site' => 's1',
-      'target_env' => 'dev',
-      'source_branch' => 'master',
-      'deployed_tag' => 'master',
-      'repo_url' => 's1@svn-3.bjaspan.hosting.acquia.com:s1.git',
-      'repo_type' => 'git',
-    ]);
-    $this->assertCommandSuccessful();
-    $this->assertCommandOutputContains('Finished updates for environment: dev');
   }
 
 }
